c079e53551b2cbf11fb4c7e1545fb96bf4be5d5f
dtroncy <damien.troncy@gmail.com>
2023-12-20 15:09:39 +0100

    Premier retour json sur tempo

--- main.go
+++ main.go
@@ -10,16 +10,26 @@ import (
 )
 
 type TempoStruct struct {
-	StartDate string              `json:"start_date"`
-	EndDate   string              `json:"end_date"`
-	Values    []TempoValuesStruct `json:"values"`
+	Data TempoDataStruct `json:"tempo_like_calendars"`
 }
 
-type TempoValuesStruct struct {
+type TempoDataStruct struct {
+	StartDate string             `json:"start_date"`
+	EndDate   string             `json:"end_date"`
+	Values    []TempoValueStruct `json:"values"`
+}
+
+type TempoValueStruct struct {
 	StartDate   string `json:"start_date"`
 	EndDate     string `json:"end_date"`
 	Value       string `json:"value"`
 	UpdatedDate string `json:"updated_date"`
+}
+
+type TempoTodayAndTomorrow struct {
+	Yesterday string `json:"yesterday"`
+	Today     string `json:"today"`
+	Tomorrow  string `json:"tomorrow"`
 }
 
 func main() {
@@ -27,6 +37,7 @@ func main() {
 	http.HandleFunc("/lastdaydata", getLastDayData)
 
 	fmt.Println("Démarrage du serveur sur le port 8080 ...")
+	fmt.Println("Aller sur http://localhost:8080/lastdaydata")
 	http.ListenAndServe(":8080", nil)
 
 }
@@ -34,13 +45,13 @@ func main() {
 func getLastDayData(w http.ResponseWriter, r *http.Request) {
 
 	today := time.Now()
+	today = time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, today.Location())
+	afterTomorrow := today.AddDate(0, 0, 2)
 	yesterday := today.AddDate(0, 0, -1)
-	formattedTodayDateTempo := today.Format("2006-01-02T15:04:05-07:00")
 	formattedYesterdayDateTempo := yesterday.Format("2006-01-02T15:04:05-07:00")
-	//formattedTodayDateConso := today.Format("2006-01-02")
-	//formattedYesterdayDateConso := yesterday.Format("2006-01-02")
+	formattedAfterTomorrowDateTempo := afterTomorrow.Format("2006-01-02T15:04:05-07:00")
 
-	tempoRequestResponse, err := tempo.GetTempoCalendar(formattedYesterdayDateTempo, formattedTodayDateTempo)
+	tempoRequestResponse, err := tempo.GetTempoCalendar(formattedYesterdayDateTempo, formattedAfterTomorrowDateTempo)
 
 	if err != nil {
 		fmt.Println("Erreur lors de la récupération des informations Tempo : ", err)
@@ -56,12 +67,27 @@ func getLastDayData(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(tempoResponse.Values[0].Value)
+	fmt.Println("Demain : " + tempoResponse.Data.Values[0].Value)
+	fmt.Println("Aujourd'hui : " + tempoResponse.Data.Values[1].Value)
+	fmt.Println("Demain : " + tempoResponse.Data.Values[2].Value)
 
-	//conso.GetDailyConsumption(formattedYesterdayDateConso, formattedTodayDateConso)
-	//conso.GetConsumptionLoadCurve(formattedYesterdayDateConso, formattedTodayDateConso)
-	//conso.GetConsumptionMaxPower(formattedYesterdayDateConso, formattedTodayDateConso)
+	tempoTodayAndTomorrow := TempoTodayAndTomorrow{
+		Yesterday: tempoResponse.Data.Values[2].Value,
+		Today:     tempoResponse.Data.Values[1].Value,
+		Tomorrow:  tempoResponse.Data.Values[0].Value,
+	}
 
+	// Encode the data as JSON
+	jsonData, err := json.Marshal(tempoTodayAndTomorrow)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the Content-Type header to application/json
 	w.Header().Set("Content-Type", "application/json")
-	//json.NewEncoder(w).Encode(data)
+
+	// Write the JSON data to the response writer
+	w.Write(jsonData)
+
 }
